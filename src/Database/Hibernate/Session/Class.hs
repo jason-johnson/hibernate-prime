4309893b894b909c63cacd1402b3f3b0328e21bd
jason <jason@johnson-net.ch>
2015-01-13 18:24:08 +0100

    Session.Class: module to make transformer interaction easier

---
+++ src/Database/Hibernate/Session/Class.hs
@@ -0,0 +1,15 @@
+module Database.Hibernate.Session.Class
+(
+  MonadSession(..)
+)
+where
+
+import Database.Hibernate.Session
+import qualified Database.Hibernate.Session as HS
+import Control.Monad.IO.Class (MonadIO)
+
+class (MonadIO m) => MonadSession m where
+  save :: (MonadIO m, Hibernatable a) => a -> m a
+
+instance (MonadIO m) => MonadSession (SessionT m) where
+  save = HS.save
